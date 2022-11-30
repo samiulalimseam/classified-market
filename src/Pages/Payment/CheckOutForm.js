18f022ab495b451fa10042b0993daa4a5dc847c2
Samiul Alim <69821457+samiulalimseam@users.noreply.github.com>
2022-11-30 23:13:54 +0600

    10

--- src/Pages/Payment/CheckOutForm.js
+++ src/Pages/Payment/CheckOutForm.js
@@ -6,7 +6,7 @@ const CheckOutForm = ({order}) => {
     const stripe = useStripe();
     const elements = useElements();
     const {productPrice} = order;
-    const data2= '';
+   
 
     const handleSubmit = async (event) => {
         event.preventDefault();
